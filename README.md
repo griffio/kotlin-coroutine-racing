8b1ac59cacc653e6253221bba81f4fc1f15ac313
griff і⊙ <346896+griffio@users.noreply.github.com>
2023-10-30 17:42:35 +0000

    project files

---
+++ README.md
@@ -0,0 +1,7 @@
+## More Kotlin Racing Ambiguous Coroutines
+
+https://griffio.github.io/programming/2023/03/18/More-Kotlin-Racing-Ambiguous-Coroutines/
+
+https://httpbin.org/
+
+Run locally `docker run -p 80:80 kennethreitz/httpbin`
---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+import kotlinx.coroutines.DEBUG_PROPERTY_NAME
+import kotlinx.coroutines.DEBUG_PROPERTY_VALUE_ON
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat.*
+import org.gradle.api.tasks.testing.logging.TestLogEvent.*
+
+plugins {
+    kotlin("jvm") version "1.9.10"
+    application
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation("io.ktor:ktor-client-okhttp:2.3.5")
+    implementation("io.ktor:ktor-client-core:2.3.5")
+    implementation("org.slf4j:slf4j-simple:2.0.7")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+    systemProperty(DEBUG_PROPERTY_NAME, DEBUG_PROPERTY_VALUE_ON)
+    testLogging {
+        showStandardStreams = true
+        exceptionFormat = FULL
+        events(STARTED, PASSED, SKIPPED, FAILED)
+    }
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+application {
+    mainClass.set("MainKt")
+}
